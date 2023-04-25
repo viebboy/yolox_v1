be50720a21aa7c1da4b180f1adbfe6c43fda3ccb
Dat Tran <viebboy@gmail.com>
2023-04-25 21:55:13 +0300

    add script to generate snpe model factory, currently testing

--- nas/to_snpe.bash
+++ nas/to_snpe.bash
@@ -3,6 +3,8 @@
 src="/home/jenkins/onnx_models"
 dst="/home/jenkins/snpe_models"
 
+cd /home/jenkins/model_conversion_tools
+
 for file in "$src"/*.onnx; do
     filename=$(basename -- "$file")
     python3 /home/jenkins/model_conversion_tools/convert.py --snpe-env snpe --snpe-fake-quantize ${file} "${dst}/${filename%.onnx}.dlc"
