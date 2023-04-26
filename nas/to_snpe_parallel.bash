b5c38b344aef0ba884f1b866a24b11d6f4363db0
Dat Tran <viebboy@gmail.com>
2023-04-26 10:28:13 +0300

    add script to generate snpe model factory, currently testing

--- nas/to_snpe_parallel.bash
+++ nas/to_snpe_parallel.bash
@@ -1,6 +1,12 @@
 
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <number_of_parallel_instances>"
+  exit 1
+fi
+
+num_parallel_instances=$1
 
 src="/home/jenkins/onnx_models"
 dst="/home/jenkins/snpe_models"
@@ -9,7 +15,7 @@ total=$(find "$src" -type f -name '*.onnx' | wc -l)
 cd /home/jenkins/model_conversion_tools
 
 find "$src" -type f -name '*.onnx' -print0 |
-  xargs -0 -P 4 -I{} sh -c '
+  xargs -0 -P $num_parallel_instances -I{} sh -c '
     file="$1"
     filename=$(basename -- "$file")
     output_dlc="${2}/${filename%.onnx}.dlc"
